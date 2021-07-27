0f3fc122b741201a2bb4537aeb1e583bb36bf453
ShaunMoran <57466726+ShaunMoran@users.noreply.github.com>
2021-07-26 19:40:42 -0500

    edited all code for assignment

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-// TODO: add code here
+window.addEventListener("load", function () {
+    
+    function buildHtml(obj) {
+        
+        let str = `
+        <div class="astronaut">
+            <div class="bio">
+                <h3>${obj.firstName + obj.lastName}</h3>
+                    <ul>
+                        <li>Hours in space: ${obj.hoursInSpace}</li>
+                        <li>Active: ${obj.active}</li>
+                        <li>Skills: ${obj.skills}</li>
+                    </ul>
+                </div>
+                <img class="avatar" src="${obj.picture}">
+        </div>`;
+        return str;
+    }
+   
+
+    
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            
+            const destination = document.getElementById("container");
+
+            for (index in json) {
+                destination.innerHTML += buildHtml(json[index]);
+            }
+        });
+    });
+
+    
+});
